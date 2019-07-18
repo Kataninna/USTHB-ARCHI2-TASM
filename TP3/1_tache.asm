731e1d8942d68b939a9904e248946082d75f50ea
Kataninna <52887480+Kataninna@users.noreply.github.com>
2019-07-18 18:18:30 +0100

    Update 1_tache.asm

--- TP3/1_tache.asm
+++ TP3/1_tache.asm
@@ -1,58 +1,72 @@
-data segment
-	message  db 10,13,"**** Programme principal en cours **** $"
-	ch1 db "oh la 1ch!... $"
-data ends
-my_stack segment stack "stack"
+DATA SEGMENT
+	deroute db "Deroutement fait....",10,13,"$"
+	message db 10,13,"**** Programme principal en cours **** $"
+	ch1 db "oh, la 1ch.... $"
+DATA ENDS
+
+MY_STACK SEGMENT STACK "stack"
 		dw 128 dup(?)
 		TOP label word
-my_stack ends
-code segment
-	assume cs: code, ds: data,ss: my_stack
-output proc near
-		mov ah,09
-		int 21h
-		ret 
-output endp
-deroute proc near
-		cmp bl,3Dh
-		Jl noaffi
-		push dx
-		mov dx,offset ch1 
-		call output
-		mov bl,0H
-		pop dx
-noaffi: iret
-deroute endp
-instalation proc near
-		push ds
-		mov ax,cs
-		mov ds,ax
-		mov dx,offset deroute
-		mov ax,251CH
-		int 21H
-		pop ds
-		ret
-instalation endp
-start:
-		MOV AX, data
-		MOV DS,AX
-		MOV AX ,my_stack
-		MOV SS,AX
-		MOV SP,TOP
-		call instalation
-		MOV AX,3 
-    	INT 10H ; clear screen grace a l'interuption 10H
-		mov bl,0
-loopa: 	mov dx,offset message ; la loop kda mena melhih ak ta3ref	
-		call output	
-		mov cx,3C0H
-lap:	
-		inc bl
-		mov ax,3d09H
-sghira:	dec ax
-		jnz sghira
-		loop lap
-		jmp loopa	
+MY_STACK ENDS
 
-code ends
-		end start
+CODE SEGMENT
+	ASSUME CS: CODE, DS: DATA , SS: MY_STACK
+	
+OUTPUT PROC NEAR          ; procédure d'affichage du message
+
+		MOV AH , 09
+		INT 21H
+		RET
+OUTPUT ENDP
+
+DEROUTER PROC NEAR                  ; procédure de déroutement qui sera dans la 1 CH
+
+		CMP BL , 3D H         ; why ? 
+		Jl NOAFFICHAGE       
+		PUSH DX
+		MOV DX, OFFSET ch1
+		CALL OUTPUT        ; affichage 
+		MOV BL , OH        ; 0 ? 
+		POP DX 
+NOAFFICHAGE:    IRET
+
+DEROUTER ENDP
+
+INSTALLATION PROC NEAR     ; installation du la nouvelle procédure dans la 1 CH 
+
+		PUSH DS 
+		MOV AX , CS
+		MOV DS , AX
+		MOV DX , OFFSET DEROUTER
+		mov ax,251CH
+		INT 21 H 
+		POP DS
+		RET
+		
+INSTALLATION ENDP 
+
+START:
+		MOV AX, DATA
+		MOV DS,AX
+		MOV AX , MY_STACK
+		MOV SS, AX
+		MOV SP, TOP
+		CALL INSTALLATION
+		MOV AX, 3 
+    	        INT 10H     ; clear screen grace a l'interruption 10H
+		MOV BL ,0
+		
+BIGLOOP: 	MOV DX , offset message ; la boucle qui fait l'affichage en continu .. 	
+		ALL OUTPUT	
+		MOV CX , 3C0H    ; 3C0H = 960 D qui représente le temps pour afficher le message "message" entre deux "message"
+lap:	
+		INC BL
+		MOB AX ,3D09 H       ; 3D09H = 15760 D qui représente le temps entre deux messages "1ch
+		
+LOOPIN:	DEC AX               ; code des deux boucles imbriquées (donné) 
+		JNZ LOOPIN
+		LOOL lap
+		JMP BIGLLOOP	
+
+CODE ENDS
+		END START
